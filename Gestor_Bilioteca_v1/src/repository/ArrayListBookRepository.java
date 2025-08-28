5de64a95b92b338723a9d0d0a3b67ece84645dd7
Ulises <perez.alvarez.ulisesss@gmail.com>
2025-08-27 19:50:05 -0600

    Repositorios finalizados

--- Gestor_Bilioteca_v1/src/repository/ArrayListBookRepository.java
+++ Gestor_Bilioteca_v1/src/repository/ArrayListBookRepository.java
@@ -1,4 +1,44 @@
 package repository;
 
-public class ArrayListBookRepository {
+import domain.Book;
+import domain.LibraryException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListBookRepository implements BookRepository{
+
+    private List<Book> books;
+
+    public ArrayListBookRepository() {
+        this.books = new ArrayList<>();
+    }
+
+    @Override
+    public void addBook(Book book) {
+        if(findBookByIsbnOrNull(book.getIsbn()) != null){
+            throw new LibraryException("El ISBN ya existe: " + book.getIsbn());
+        }
+        books.add(book);
+    }
+
+    @Override
+    public void removeBook(String isbn) {
+        Book book = findBookByIsbn(isbn);
+        books.remove(book);
+    }
+
+    @Override
+    public Book findBookByIsbn(String isbn) {
+        return books.stream().filter(b -> b.getIsbn().equals(isbn)).findFirst().orElseThrow(() -> new LibraryException("Libro no encontrado con ISBN: " + isbn));
+    }
+
+    private Book findBookByIsbnOrNull(String isbn){
+        return books.stream().filter(book -> book.getIsbn().equals(isbn)).findFirst().orElseThrow(null);
+    }
+
+    @Override
+    public List<Book> getAllBooks() {
+        return new ArrayList<>(books);
+    }
 }
--- Gestor_Bilioteca_v1/src/repository/BookRepository.java
+++ Gestor_Bilioteca_v1/src/repository/BookRepository.java
@@ -1,4 +1,12 @@
 package repository;
 
-public class BookRepository {
+import domain.Book;
+
+import java.util.List;
+
+public interface BookRepository {
+    void addBook(Book book);
+    void removeBook(String isbn);
+    Book findBookByIsbn(String isbn);
+    List<Book> getAllBooks();
 }
