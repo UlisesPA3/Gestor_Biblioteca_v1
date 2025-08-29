3a5a3ae250b85eb620612d9cf1d730e705739ed2
Ulises <perez.alvarez.ulisesss@gmail.com>
2025-08-28 20:39:28 -0600

    Servicios finalizados

--- Gestor_Bilioteca_v1/src/service/BookFilterService.java
+++ Gestor_Bilioteca_v1/src/service/BookFilterService.java
@@ -1,4 +1,20 @@
 package service;
 
+import domain.Book;
+import filter.BookFilter;
+import repository.BookRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BookFilterService {
+    private final BookRepository bookRepository;
+
+    public BookFilterService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> filterBooks(BookFilter filter){
+        return bookRepository.getAllBooks().stream().filter(filter::apply).collect(Collectors.toList());
+    }
 }
--- Gestor_Bilioteca_v1/src/service/LoanManager.java
+++ Gestor_Bilioteca_v1/src/service/LoanManager.java
@@ -1,4 +1,28 @@
 package service;
 
+import domain.Book;
+import domain.LibraryException;
+import repository.BookRepository;
+
 public class LoanManager {
+    private final BookRepository booksRepository;
+
+    public LoanManager(BookRepository booksRepository) {
+        this.booksRepository = booksRepository;
+    }
+
+    public void lendBook(String isbn){
+        Book book = booksRepository.findBookByIsbn(isbn);
+            if(!book.isAvailable()){
+                throw new LibraryException("El libro ya esta prestado: " + isbn);
+            }
+            book.setAvailable(false);
+        }
+    public void returnBook(String isbn){
+        Book book = booksRepository.findBookByIsbn(isbn);
+        if(book.isAvailable()){
+            throw new LibraryException("El libro ya esta disponible, no puede devolverse" + isbn);
+        }
+        book.setAvailable(true);
+    }
 }
